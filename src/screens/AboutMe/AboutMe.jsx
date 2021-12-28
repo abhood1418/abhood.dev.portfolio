3e76d2790633dc22b8c928849ac404fefefc94b3
Andrew Hood <andrewhood@Andrews-MacBook-Pro.local>
2021-12-28 17:51:45 -0500

    construction styling

--- src/screens/AboutMe/AboutMe.jsx
+++ src/screens/AboutMe/AboutMe.jsx
@@ -1,9 +1,11 @@
 
 import firemage from "../../Assets/firemage.gif";
+import "./AboutMe.css";
+
 
 const AboutMe = () => {
   return (
-    <div>
+    <div className="construction">
       <h1>Under Construction</h1>
       <img src={firemage} />
     </div>
